8e5553714f285601d4300a4f6fb47a1f39cf579f
MauroGuidi <79712385+MauroGuidi@users.noreply.github.com>
2022-10-17 17:13:49 +0200

    Debug github object.

--- dist/index.js
+++ dist/index.js
@@ -1,127 +1,6 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ 458:
-/***/ ((module, __webpack_exports__, __nccwpck_require__) => {
-
-"use strict";
-__nccwpck_require__.a(module, async (__webpack_handle_async_dependencies__) => {
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(312);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__nccwpck_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(3370);
-/* harmony import */ var _actions_github__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__nccwpck_require__.n(_actions_github__WEBPACK_IMPORTED_MODULE_1__);
-
-
-
-try {
-  const [repoOwner, repo] = (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput)("repo").split("/");
-  const wfRunId = (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput)("run-id");
-  const slackWebhook = (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.getInput)("slack-webhook");
-
-  console.log("Actions: ", _actions_github__WEBPACK_IMPORTED_MODULE_1__.actions);
-  console.log("Request: ", _actions_github__WEBPACK_IMPORTED_MODULE_1__.request);
-
-  // Retrieve workflow run data
-  const wfRun = await _actions_github__WEBPACK_IMPORTED_MODULE_1__.actions.getWorkflowRun({
-    owner: repoOwner,
-    repo: repo,
-    run_id: wfRunId,
-  });
-  const jobsResponse = await (0,_actions_github__WEBPACK_IMPORTED_MODULE_1__.request)(wfRun.data.jobs_url);
-
-  // Parse data for message fields
-  const commitAuthor = wfRun.data.actor.login;
-  const avatarUrl = wfRun.data.actor.avatarUrl;
-  const repositoryUrl = wfRun.data.repository.html_url;
-  const repositoryName = wfRun.data.repository.full_name;
-  const branchName = wfRun.data.head_branch;
-  const branchUrl = repositoryUrl + "/tree/" + branchName;
-  const commitSha = wfRun.data.head_sha;
-  const commitUrl = repositoryUrl + "/commit/" + commitSha;
-  const commitMessage = wfRun.data.display_title;
-
-  const failedJobs = jobsResponse.data.jobs.filter(
-    (job) => job.status == "completed" && job.conclusion == "failure"
-  );
-  const failedJobsWithLinks = failedJobs
-    .map((job) => `<${job.html_url}|${job.name}>`)
-    .join(", ");
-
-  const slackMessage = {
-    attachments: [
-      {
-        color: `${failedJobs.length == 0 ? "#36a64f" : "#8B0000"}`,
-        blocks: [
-          {
-            type: "context",
-            elements: [
-              {
-                type: "image",
-                image_url: avatarUrl,
-                alt_text: "cute cat",
-              },
-              {
-                type: "mrkdwn",
-                text: "*" + commitAuthor + "*",
-              },
-            ],
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `<${repositoryUrl}|*${repositoryName}*>`,
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `*Branch:* <${branchUrl}|${branchName}>`,
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `*Commit:* ${commitMessage} (<${commitUrl}|${commitSha}>)`,
-            },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text:
-                failedJobs.length == 0
-                  ? "All tests passed :rocket:"
-                  : `*Failed job${
-                      failedJobs.length > 1 ? "s" : ""
-                    }:* ${failedJobsWithLinks}`,
-            },
-          },
-        ],
-      },
-    ],
-  };
-
-  // For testing on Slack Block Kit Builder:
-  console.log("Slack message: ", JSON.stringify(slackMessage));
-
-  // Send message to Slack
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", slackWebhook, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.send(JSON.stringify(slackMessage));
-} catch (error) {
-  (0,_actions_core__WEBPACK_IMPORTED_MODULE_0__.setFailed)(error.message);
-}
-
-__webpack_handle_async_dependencies__();
-}, 1);
-
-/***/ }),
-
 /***/ 5996:
 /***/ (function(__unused_webpack_module, exports, __nccwpck_require__) {
 
@@ -9797,131 +9676,121 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/async module */
-/******/ 	(() => {
-/******/ 		var webpackThen = typeof Symbol === "function" ? Symbol("webpack then") : "__webpack_then__";
-/******/ 		var webpackExports = typeof Symbol === "function" ? Symbol("webpack exports") : "__webpack_exports__";
-/******/ 		var completeQueue = (queue) => {
-/******/ 			if(queue) {
-/******/ 				queue.forEach((fn) => (fn.r--));
-/******/ 				queue.forEach((fn) => (fn.r-- ? fn.r++ : fn()));
-/******/ 			}
-/******/ 		}
-/******/ 		var completeFunction = (fn) => (!--fn.r && fn());
-/******/ 		var queueFunction = (queue, fn) => (queue ? queue.push(fn) : completeFunction(fn));
-/******/ 		var wrapDeps = (deps) => (deps.map((dep) => {
-/******/ 			if(dep !== null && typeof dep === "object") {
-/******/ 				if(dep[webpackThen]) return dep;
-/******/ 				if(dep.then) {
-/******/ 					var queue = [];
-/******/ 					dep.then((r) => {
-/******/ 						obj[webpackExports] = r;
-/******/ 						completeQueue(queue);
-/******/ 						queue = 0;
-/******/ 					});
-/******/ 					var obj = {};
-/******/ 												obj[webpackThen] = (fn, reject) => (queueFunction(queue, fn), dep['catch'](reject));
-/******/ 					return obj;
-/******/ 				}
-/******/ 			}
-/******/ 			var ret = {};
-/******/ 								ret[webpackThen] = (fn) => (completeFunction(fn));
-/******/ 								ret[webpackExports] = dep;
-/******/ 								return ret;
-/******/ 		}));
-/******/ 		__nccwpck_require__.a = (module, body, hasAwait) => {
-/******/ 			var queue = hasAwait && [];
-/******/ 			var exports = module.exports;
-/******/ 			var currentDeps;
-/******/ 			var outerResolve;
-/******/ 			var reject;
-/******/ 			var isEvaluating = true;
-/******/ 			var nested = false;
-/******/ 			var whenAll = (deps, onResolve, onReject) => {
-/******/ 				if (nested) return;
-/******/ 				nested = true;
-/******/ 				onResolve.r += deps.length;
-/******/ 				deps.map((dep, i) => (dep[webpackThen](onResolve, onReject)));
-/******/ 				nested = false;
-/******/ 			};
-/******/ 			var promise = new Promise((resolve, rej) => {
-/******/ 				reject = rej;
-/******/ 				outerResolve = () => (resolve(exports), completeQueue(queue), queue = 0);
-/******/ 			});
-/******/ 			promise[webpackExports] = exports;
-/******/ 			promise[webpackThen] = (fn, rejectFn) => {
-/******/ 				if (isEvaluating) { return completeFunction(fn); }
-/******/ 				if (currentDeps) whenAll(currentDeps, fn, rejectFn);
-/******/ 				queueFunction(queue, fn);
-/******/ 				promise['catch'](rejectFn);
-/******/ 			};
-/******/ 			module.exports = promise;
-/******/ 			body((deps) => {
-/******/ 				if(!deps) return outerResolve();
-/******/ 				currentDeps = wrapDeps(deps);
-/******/ 				var fn, result;
-/******/ 				var promise = new Promise((resolve, reject) => {
-/******/ 					fn = () => (resolve(result = currentDeps.map((d) => (d[webpackExports]))));
-/******/ 					fn.r = 0;
-/******/ 					whenAll(currentDeps, fn, reject);
-/******/ 				});
-/******/ 				return fn.r ? promise : result;
-/******/ 			}).then(outerResolve, reject);
-/******/ 			isEvaluating = false;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__nccwpck_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__nccwpck_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module used 'module' so it can't be inlined
-/******/ 	var __webpack_exports__ = __nccwpck_require__(458);
-/******/ 	module.exports = __webpack_exports__;
-/******/ 	
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+const core = __nccwpck_require__(312);
+const github = __nccwpck_require__(3370);
+
+try {
+  // const [repoOwner, repo] = getInput("repo").split("/");
+  // const wfRunId = getInput("run-id");
+  // const slackWebhook = getInput("slack-webhook");
+
+  console.log(github.context);
+
+  // Retrieve workflow run data
+  // const wfRun = await actions.getWorkflowRun({
+  //   owner: repoOwner,
+  //   repo: repo,
+  //   run_id: wfRunId,
+  // });
+  // const jobsResponse = await request(wfRun.data.jobs_url);
+
+  // // Parse data for message fields
+  // const commitAuthor = wfRun.data.actor.login;
+  // const avatarUrl = wfRun.data.actor.avatarUrl;
+  // const repositoryUrl = wfRun.data.repository.html_url;
+  // const repositoryName = wfRun.data.repository.full_name;
+  // const branchName = wfRun.data.head_branch;
+  // const branchUrl = repositoryUrl + "/tree/" + branchName;
+  // const commitSha = wfRun.data.head_sha;
+  // const commitUrl = repositoryUrl + "/commit/" + commitSha;
+  // const commitMessage = wfRun.data.display_title;
+
+  // const failedJobs = jobsResponse.data.jobs.filter(
+  //   (job) => job.status == "completed" && job.conclusion == "failure"
+  // );
+  // const failedJobsWithLinks = failedJobs
+  //   .map((job) => `<${job.html_url}|${job.name}>`)
+  //   .join(", ");
+
+  // const slackMessage = {
+  //   attachments: [
+  //     {
+  //       color: `${failedJobs.length == 0 ? "#36a64f" : "#8B0000"}`,
+  //       blocks: [
+  //         {
+  //           type: "context",
+  //           elements: [
+  //             {
+  //               type: "image",
+  //               image_url: avatarUrl,
+  //               alt_text: "cute cat",
+  //             },
+  //             {
+  //               type: "mrkdwn",
+  //               text: "*" + commitAuthor + "*",
+  //             },
+  //           ],
+  //         },
+  //         {
+  //           type: "section",
+  //           text: {
+  //             type: "mrkdwn",
+  //             text: `<${repositoryUrl}|*${repositoryName}*>`,
+  //           },
+  //         },
+  //         {
+  //           type: "section",
+  //           text: {
+  //             type: "mrkdwn",
+  //             text: `*Branch:* <${branchUrl}|${branchName}>`,
+  //           },
+  //         },
+  //         {
+  //           type: "section",
+  //           text: {
+  //             type: "mrkdwn",
+  //             text: `*Commit:* ${commitMessage} (<${commitUrl}|${commitSha}>)`,
+  //           },
+  //         },
+  //         {
+  //           type: "section",
+  //           text: {
+  //             type: "mrkdwn",
+  //             text:
+  //               failedJobs.length == 0
+  //                 ? "All tests passed :rocket:"
+  //                 : `*Failed job${
+  //                     failedJobs.length > 1 ? "s" : ""
+  //                   }:* ${failedJobsWithLinks}`,
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // };
+
+  // // For testing on Slack Block Kit Builder:
+  // console.log("Slack message: ", JSON.stringify(slackMessage));
+
+  // // Send message to Slack
+  // var xhr = new XMLHttpRequest();
+  // xhr.open("POST", slackWebhook, true);
+  // xhr.setRequestHeader("Content-Type", "application/json");
+  // xhr.send(JSON.stringify(slackMessage));
+} catch (error) {
+  setFailed(error.message);
+}
+
+})();
+
+module.exports = __webpack_exports__;
 /******/ })()
 ;
