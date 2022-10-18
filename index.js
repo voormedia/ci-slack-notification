966d70534167aa5a81abcf22147f76f155f04fc2
MauroGuidi <79712385+MauroGuidi@users.noreply.github.com>
2022-10-18 15:09:56 +0200

    Use Octokit instead of tapping directly on the API.

--- index.js
+++ index.js
@@ -1,105 +1,125 @@
-const core = require("@actions/core");
 // const github = require("@actions/github"); // Not sure if we need this if using API
-const { Octokit } = require("@octokit/core");
+import * as core from "@actions/core";
+import { context, getOctokit } from "@actions/github";
 
 async function run() {
   const token = core.getInput("token");
-  const [repoOwner, repo] = core.getInput("repo").split("/");
-  const wfRunId = core.getInput("run-id");
   const slackWebhook = core.getInput("slack-webhook");
 
-  const wfRunUrl = `/repos/${repoOwner}/${repo}/actions/runs/${wfRunId}`;
+  // Force as secret, forces *** when trying to print or log values
+  core.setSecret(token);
+  core.setSecret(slackWebhook);
 
-  const octokit = new Octokit({ auth: token });
+  const repoOwner = context.repo.owner;
+  const repo = context.repo.repo;
+  const wfRunId = context.runId;
 
-  // Use Github API to fetch workflow run & jobs data
-  const wfRun = await octokit.request(`GET ${wfRunUrl}`);
-  const jobsResponse = await octokit.request(`GET ${wfRunUrl}/jobs`);
+  // Authenticate with GitHub
+  const octokit = getOctokit(token);
+
+  // Fetch workflow run data
+  const { data: wfRun } = await octokit.actions.getWorkflowRun({
+    owner: repoOwner,
+    repo: repo,
+    run_id: wfRunId,
+  });
+
+  // Fetch jobs data for the same workflow run
+  const { data: jobsResponse } = await octokit.actions.listJobsForWorkflowRun({
+    owner: repoOwner,
+    repo: repo,
+    run_id: wfRunId,
+  });
 
   console.log(wfRun);
   console.log(jobsResponse);
 
-  // const commitAuthor = wfRun.data.actor.login;
-  // const avatarUrl = wfRun.data.actor.avatar_url;
-  // const repositoryUrl = wfRun.data.repository.html_url;
-  // const repositoryName = wfRun.data.repository.full_name;
-  // const branchName = wfRun.data.head_branch;
-  // const branchUrl = repositoryUrl + "/tree/" + branchName;
-  // const commitSha = wfRun.data.head_sha;
-  // const commitUrl = repositoryUrl + "/commit/" + commitSha;
-  // const commitMessage = wfRun.data.display_title;
-  // const failedJobs = jobsResponse.data.jobs.filter(
-  //   (job) => job.status == "completed" && job.conclusion == "failure"
-  // );
-  // const failedJobsWithLinks = failedJobs
-  //   .map((job) => `<${job.html_url}|${job.name}>`)
-  //   .join(", ");
+  // Extract the data we need for creating the Slack message
+  const commitAuthor = wfRun.data.actor.login;
+  const avatarUrl = wfRun.data.actor.avatar_url;
+  const repositoryUrl = wfRun.data.repository.html_url;
+  const repositoryName = wfRun.data.repository.full_name;
+  const branchName = wfRun.data.head_branch;
+  const branchUrl = repositoryUrl + "/tree/" + branchName;
+  const commitSha = wfRun.data.head_sha;
+  const commitUrl = repositoryUrl + "/commit/" + commitSha;
+  const commitMessage = wfRun.data.display_title;
+  const failedJobs = jobsResponse.data.jobs.filter(
+    (job) => job.status == "completed" && job.conclusion == "failure"
+  );
+  const failedJobsWithLinks = failedJobs
+    .map((job) => `<${job.html_url}|${job.name}>`)
+    .join(", ");
 
-  // const slackMessage = {
-  //   attachments: [
-  //     {
-  //       color: `${failedJobs.length == 0 ? "#36a64f" : "#8B0000"}`,
-  //       blocks: [
-  //         {
-  //           type: "context",
-  //           elements: [
-  //             {
-  //               type: "image",
-  //               image_url: avatarUrl,
-  //               alt_text: "cute cat",
-  //             },
-  //             {
-  //               type: "mrkdwn",
-  //               text: "*" + commitAuthor + "*",
-  //             },
-  //           ],
-  //         },
-  //         {
-  //           type: "section",
-  //           text: {
-  //             type: "mrkdwn",
-  //             text: `<${repositoryUrl}|*${repositoryName}*>`,
-  //           },
-  //         },
-  //         {
-  //           type: "section",
-  //           text: {
-  //             type: "mrkdwn",
-  //             text: `*Branch:* <${branchUrl}|${branchName}>`,
-  //           },
-  //         },
-  //         {
-  //           type: "section",
-  //           text: {
-  //             type: "mrkdwn",
-  //             text: `*Commit:* ${commitMessage} (<${commitUrl}|${commitSha}>)`,
-  //           },
-  //         },
-  //         {
-  //           type: "section",
-  //           text: {
-  //             type: "mrkdwn",
-  //             text:
-  //               failedJobs.length == 0
-  //                 ? "All tests passed :rocket:"
-  //                 : `*Failed job${
-  //                     failedJobs.length > 1 ? "s" : ""
-  //                   }:* ${failedJobsWithLinks}`,
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // };
+  // Create Slack message
+  const slackMessage = {
+    attachments: [
+      {
+        color: `${failedJobs.length == 0 ? "good" : "danger"}`,
+        blocks: [
+          {
+            type: "context",
+            elements: [
+              {
+                type: "image",
+                image_url: avatarUrl,
+                alt_text: "cute cat",
+              },
+              {
+                type: "mrkdwn",
+                text: "*" + commitAuthor + "*",
+              },
+            ],
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `<${repositoryUrl}|*${repositoryName}*>`,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `*Branch:* <${branchUrl}|${branchName}>`,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: `*Commit:* ${commitMessage} (<${commitUrl}|${commitSha}>)`,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text:
+                failedJobs.length == 0
+                  ? "All tests passed :rocket:"
+                  : `*Failed job${
+                      failedJobs.length > 1 ? "s" : ""
+                    }:* ${failedJobsWithLinks}`,
+            },
+          },
+        ],
+      },
+    ],
+  };
 
-  // // For testing on Slack Block Kit Builder:
+  // For testing on Slack Block Kit Builder:
   // console.log("Slack message: ", JSON.stringify(slackMessage));
 
-  // // Send message to Slack
-  // var xhr = new XMLHttpRequest();
-  // xhr.open("POST", slackWebhook, true);
-  // xhr.setRequestHeader("Content-Type", "application/json");
-  // xhr.send(JSON.stringify(slackMessage));
+  // Send message to Slack
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", slackWebhook, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify(slackMessage));
 }
 
-run().catch((error) => core.setFailed(error.message));
+run().catch((error) => {
+  core.setFailed(error.message);
+  console.error(error.stack);
+});
